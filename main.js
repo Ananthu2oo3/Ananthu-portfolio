c573a01297b490dc1363f47216722bebc7f14965
Ananthu2oo3 <ananthug.2003@gmail.com>
2023-01-27 18:10:21 +0530

    added js

---
+++ main.js
@@ -0,0 +1,18 @@
+var scrollButton = document.getElementById("scroll-about");
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("pic");
+element.scrollIntoView({behavior: "smooth"});
+});
+
+var scrollButton = document.getElementById("scroll-skill");      
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("skill");
+element.scrollIntoView({behavior: "smooth"});
+}); 
+
+var scrollButton = document.getElementById("scroll-blogs");
+scrollButton.addEventListener("click", function() {
+var element = document.getElementById("art");
+element.scrollIntoView({behavior: "smooth"});
+});
+
